c9f655ed13d4dd431653f996e66a1a0b35d99179
GonCalvo <93337673+GonCalvo@users.noreply.github.com>
2022-05-12 13:37:13 +0200

    added scifi look to target range

--- Assets/Scripts/TargetManager.cs
+++ Assets/Scripts/TargetManager.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using Valve.VR.Extras;
 
-public class TargetManager : MonoBehaviour, Manager
+public class TargetManager : MonoBehaviour
 {
 
     public SteamVR_LaserPointer laserPointer;
+    public GameObject SCORE_BOARD;
     public GameObject target;
+    public GameObject explosion;
     public int total_targets = 10;
     public float max_delta_x = 2;
     public float max_delta_y = 10;
     public float time_between_targets = 5;
 
     private float last_target_rez_time = 0;
-    private const float z_dist = 10f;
+    private const float z_dist = 18.5f;
+
+    private bool finished = false;
+    private Transform cam_transform;
 
     private ScoreManager score_manager;
 
@@ -25,13 +31,33 @@ public class TargetManager : MonoBehaviour, Manager
         MoveTarget();
     }
 
+    private void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform currentItem = transform.GetChild(i);
+            if (currentItem.name.Equals("Camera"))
+            {
+                cam_transform = currentItem.transform;
+            }
+        }
+
+        var particles = target.transform.GetChild(0).GetComponent<ParticleSystem>().emission;
+        particles.enabled = false;
+    }
+
     public void PointerInside(object sender, PointerEventArgs e)
     {
         if (e.target.transform == target.transform)
         {
             //Destroy(e.target.gameObject);
-            score_manager.Score(true);
-            MoveTarget();
+            if ( ! finished )
+            {
+                score_manager.Score(true);
+                GameObject _explosion = Instantiate( explosion, target.transform);
+                Destroy(_explosion, 1f);
+                MoveTarget();
+            }
         }
     }
 
@@ -54,7 +80,13 @@ public class TargetManager : MonoBehaviour, Manager
 
     private void Finish()
     {
-
+        finished = true;
+        GameObject scoreboard = Instantiate(SCORE_BOARD);
+        ScoreBoard scoreboard_script = scoreboard.GetComponent<ScoreBoard>();
+        scoreboard_script.camera_transform = cam_transform;
+        scoreboard_script.score = score_manager.score;
+        scoreboard_script.targets = total_targets;
+        scoreboard_script.max_spree = score_manager.max_streak;
     }
 }
 
